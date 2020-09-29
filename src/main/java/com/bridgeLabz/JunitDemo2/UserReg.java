6b6f4dd4095823479b0c8ed476348dcc2eb2762f
unknown <ompreetam317@gmail.com>
2020-09-29 18:42:13 +0530

    [Preetam]Added: Two Test cases for Validating Last name feature

--- src/main/java/com/bridgeLabz/JunitDemo2/UserReg.java
+++ src/main/java/com/bridgeLabz/JunitDemo2/UserReg.java
@@ -13,6 +13,16 @@ public class UserReg {
 		}
 		return "in-valid";
 	}
+	
+	public String LastName(String s) {
+		String regex = "^([A-Z]{1})([a-z]{2,})$";
+	     
+	    boolean result = Pattern.matches(regex,s);
+		if(result) {
+			return "valid";
+		}
+		return "in-valid";
+	}
 
 	
 }
--- src/test/java/com/bridgeLabz/JunitDemo2/UserRegTest.java
+++ src/test/java/com/bridgeLabz/JunitDemo2/UserRegTest.java
@@ -16,8 +16,8 @@ public class UserRegTest {
 	public void GIVEN_FIRST_NAME_STARTS_CAP_AND_CONTAINS_3_OR_MORE_LETTERS() {
 		
 		UserReg user = new UserReg();
-		String Name = user.FirstName("Preetam");
-		assertEquals("valid", Name);
+		String FName = user.FirstName("Preetam");
+		assertEquals("valid", FName);
 		
 	} 
 	
@@ -29,5 +29,26 @@ public class UserRegTest {
 		assertEquals("in-valid", Name);
 		
 	} 
+	
+	
+	@Test
+	public void GIVEN_LAST_NAME_STARTS_CAP_AND_CONTAINS_3_OR_MORE_LETTERS() {
+		
+		UserReg user = new UserReg();
+		String Name = user.FirstName("Mukhopadhyay");
+		assertEquals("valid", Name);
+		
+	} 
+	
+	@Test
+	public void GIVEN_LAST_NAME_DOES_NOT_STARTS_CAP_AND_CONTAINS_3_OR_MORE_LETTERS() {
+		
+		UserReg user = new UserReg();
+		String LName = user.FirstName("pk");
+		assertEquals("in-valid", LName);
+		
+	} 
+	
+	
 
 }
