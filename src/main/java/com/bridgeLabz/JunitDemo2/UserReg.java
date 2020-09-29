81c05fe5c6ffa8b4992a35f40fd1143d95b661c9
unknown <ompreetam317@gmail.com>
2020-09-29 18:30:49 +0530

    [Preetam]Added:Welcome Message

---
+++ src/main/java/com/bridgeLabz/JunitDemo2/UserReg.java
@@ -0,0 +1,18 @@
+package com.bridgeLabz.JunitDemo2;
+
+import java.util.regex.Pattern;
+
+public class UserReg {
+
+	public String FirstName(String s) {
+		String regex = "^([A-Z]{1})([a-z]{2,})$";
+	     
+	    boolean result = Pattern.matches(regex,s);
+		if(result) {
+			return "valid";
+		}
+		return "in-valid";
+	}
+
+	
+}
---
+++ src/test/java/com/bridgeLabz/JunitDemo2/UserRegTest.java
@@ -0,0 +1,15 @@
+package com.bridgeLabz.JunitDemo2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class UserRegTest {
+	
+	public void Welcome() {
+		
+		System.out.println("Welcome to JUnit Testing of User Registeration");
+	}
+
+}
