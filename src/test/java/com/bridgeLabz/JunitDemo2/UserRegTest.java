ba3665b4ed067e1760daae34728d49d0047b9d70
unknown <ompreetam317@gmail.com>
2020-09-29 18:36:59 +0530

    [Preetam]Added: Two Test cases for Validating First name feature

--- src/test/java/com/bridgeLabz/JunitDemo2/UserRegTest.java
+++ src/test/java/com/bridgeLabz/JunitDemo2/UserRegTest.java
@@ -11,5 +11,23 @@ public class UserRegTest {
 		
 		System.out.println("Welcome to JUnit Testing of User Registeration");
 	}
+	
+	@Test
+	public void GIVEN_FIRST_NAME_STARTS_CAP_AND_CONTAINS_3_OR_MORE_LETTERS() {
+		
+		UserReg user = new UserReg();
+		String Name = user.FirstName("Preetam");
+		assertEquals("valid", Name);
+		
+	} 
+	
+	@Test
+	public void GIVEN_FIRST_NAME_DOES_NOT_STARTS_CAP_AND_CONTAINS_3_OR_MORE_LETTERS() {
+		
+		UserReg user = new UserReg();
+		String Name = user.FirstName("pk");
+		assertEquals("in-valid", Name);
+		
+	} 
 
 }
