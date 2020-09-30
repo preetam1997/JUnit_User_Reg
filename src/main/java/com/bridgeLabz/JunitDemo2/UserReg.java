80001857a9f62ed6c124990b261a40fa59f83dad
unknown <ompreetam317@gmail.com>
2020-09-30 22:57:41 +0530

    [Preetam]Added: UC-11 paremeterised testing for emails

--- src/main/java/com/bridgeLabz/JunitDemo2/UserReg.java
+++ src/main/java/com/bridgeLabz/JunitDemo2/UserReg.java
@@ -63,6 +63,19 @@ public class UserReg {
 		}
 		
 	   }
+	
+	public String EmailValidatorForParameterisedTesting(String s) {
+		
+		 String regex = "^(?!.*\\.{2})([a-zA-Z0-9]+)([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]?)([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-z A-Z]{2,20}).([a-z A-Z]{2,4})(.[a-z]{2,8})?$";
+	     
+		 boolean result = Pattern.matches(regex,s);
+		 	if(result) {
+		 		return "valid";
+		 		}
+			return "in-valid";
+		}
+	
+	
 
 	public String PhoneNumber(String s) throws UserRegExecption {
 		String regex = "^([0-9]{2})(\\s)([0-9]{10})$";
---
+++ src/test/java/com/bridgeLabz/JunitDemo2/ParametricEmailTester.java
@@ -0,0 +1,64 @@
+package com.bridgeLabz.JunitDemo2;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ParametricEmailTester {
+	
+	private String email;
+	private String result;
+	
+	private UserReg user;
+	
+	public ParametricEmailTester(String email,String result) {
+		
+		this.email = email;
+		this.result = result;
+	}
+	
+	@Before
+	public void initialize() {
+		user = new UserReg();
+	}
+	
+	@Parameterized.Parameters
+	public static Collection input() {
+		
+		return Arrays.asList(new Object[][] {
+			{"abc@yahoo.com","valid"},
+			{"abc-100@yahoo.com","valid"},
+			{"abc.100@gmail.com","valid"},
+			{"abc111@abc.com","valid"},
+			{"abc-100@abc.net","valid"},
+			{"abc.100@abc.com.au","valid"},
+			{"abc@gmail.com.com","valid"},
+			{"abc+100@gmail.com","valid"},
+			{"abc","in-valid"},
+			{"abc@.com.my","in-valid"},
+			{"abc123@gmail.a","in-valid"},
+			{".abc@abc.com","in-valid"},
+			{"abc()*@gmail.com.com","in-valid"},
+			{"abc@%*.com.com","in-valid"},
+			{"abc..2002@gmail.com","in-valid"},
+			{"abc.@gmail.com","in-valid"},
+			{"abc@abc@gmail.com","in-valid"},
+			{"abc@gmail.com.1a","in-valid"},
+			{"abc@gmail.com.aa.au","in-valid"},
+			{"abc@1.com","in-valid"}});
+	}
+	
+	@Test
+	public void testEmail(){
+		assertEquals(result, user.EmailValidatorForParameterisedTesting(email));
+		
+	}
+
+}
